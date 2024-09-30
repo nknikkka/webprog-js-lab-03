8ef6781f7f2a923ab5794641d9db15e5d16a12bb
nknikkka <kulikveronika14@gmail.com>
2024-09-30 19:45:25 +0300

    Оновлення файлів task-1.js, task-2.js, task-3.js, task-4.js

---
+++ JS/task-1.js
@@ -0,0 +1,18 @@
+// Оголошення функції makeTransaction
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    const totalPrice = quantity * pricePerDroid;
+    if (totalPrice > customerCredits) {
+      return "Insufficient funds!";
+    } else {
+      return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    }
+  }
+  
+  document.getElementById("task1-result").innerHTML = `
+    <p>${makeTransaction(5, 3000, 23000)}</p>
+    <p>${makeTransaction(3, 1000, 15000)}</p>
+    <p>${makeTransaction(10, 5000, 8000)}</p>
+    <p>${makeTransaction(8, 2000, 10000)}</p>
+    <p>${makeTransaction(10, 500, 5000)}</p>
+  `;
+  
